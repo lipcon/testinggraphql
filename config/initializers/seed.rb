8fe2eb78ee4f87cacf79bee1205c2a3a44d647ae
Constantin <con.lipcaneanu@gmail.com>
2020-06-11 17:09:45 +0300

    seed

---
+++ config/initializers/seed.rb
@@ -0,0 +1,5 @@
+Person.create name: "aliona smith", age: 23
+Person.create name: "vasile smith", age: 23
+Person.create name: "luca smith", age: 35
+Person.create name: "luca cons", age: 33
+Person.create name: "john cons", age: 3
